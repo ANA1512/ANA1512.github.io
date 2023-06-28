807fcaa0e02ad07b12a2eba3e38152c62ed48495
ANA1512 <65736679+ANA1512@users.noreply.github.com>
2023-06-28 11:58:35 +0200

    Add files via upload

---
+++ utils/styles/atoms.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+
+export const StyledLink = styled(Link)`
+  font-family: Montserrat;
+  color: #FF6060;
+  text-decoration: none;
+  font-weight: 500;
+  text-align: center;
+  
+`
