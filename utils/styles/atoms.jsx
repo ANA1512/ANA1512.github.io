54b5bad8bb050b2671e6fc6a9911fabc86360dfc
ANA1512 <65736679+ANA1512@users.noreply.github.com>
2023-07-08 11:05:52 +0200

    Add files via upload

--- utils/styles/atoms.jsx
+++ utils/styles/atoms.jsx
@@ -4,9 +4,32 @@ import styled from 'styled-components'
 
 export const StyledLink = styled(Link)`
   font-family: Montserrat;
-  color: #FF6060;
+
   text-decoration: none;
   font-weight: 500;
   text-align: center;
+  color: #FF6060;
+  text-transform: uppercase;
+  &:hover{
+    text-decoration: underline;
+  }
+
+  @media (min-width: 1200px) {
+  
+     text-transform: lowercase;
+      &:hover{
+      text-decoration: underline;
+    }
+
+  }
   
 `
+
+export const StyledSyntaxe = styled.p`
+font-family: Montserrat;
+color: #FF6060;
+
+
+
+`
+
