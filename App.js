758ebafd69a28c6078b7049f256014f1192b757e
ANA1512 <65736679+ANA1512@users.noreply.github.com>
2023-07-08 09:51:22 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Erreur from './components/Erreur/index'
+import Home from  './pages/Home'
+import Apropos from'./pages/Apropos'
+import Logement from './pages/Logement'
+import './App.css'
 
 
 function App() {
   return (
     <div className="App">
-     
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Erreur />} />
+        <Route path="/apropos" element={<Apropos/>} />
+        <Route path="/logement/:id" element={<Logement/>} />
+      </Routes>
+    </Router>
+   
     
     </div>
   );
